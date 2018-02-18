562efbb12db9761776ad586c6d287341cf835a17
yangfan <hyyangfan@gmail.com>
2018-02-18 22:18:52 +0800

    init

---
+++ src/main/java/com/roncoo/eshop/inventory/EshopInventoryApplication.java
@@ -0,0 +1,12 @@
+package com.roncoo.eshop.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopInventoryApplication.class, args);
+	}
+}
